77bd892ece4f9d1b42a59c0cb3ecdd24d1444e70
Shoichi Imamura <imasho@gmail.com>
2013-12-03 22:15:55 +0900

    fix default test

---
+++ sandbox/sandboxTests/sandboxTests.m
@@ -0,0 +1,34 @@
+//
+//  sandboxTests.m
+//  sandbox
+//
+//  Created by 今村 庄一 on 2013/12/03.
+//  Copyright (c) 2013年 HANDLEK. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sandboxTests : XCTestCase
+
+@end
+
+@implementation sandboxTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertTrue(true);
+}
+
+@end
