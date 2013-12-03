acf3e12c0e253c2c8f656e901ec0ba3e811c981c
Shoichi Imamura <imasho@gmail.com>
2013-12-03 21:47:55 +0900

    sample data model and test

---
+++ sandbox/sandbox/DataModel/GenreData.h
@@ -0,0 +1,17 @@
+//
+//  GenreData.h
+//  sandbox
+//
+//  Created by 今村 庄一 on 2013/12/03.
+//  Copyright (c) 2013年 HANDLEK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GenreData : NSObject
+
+@property (nonatomic, assign) NSInteger genreId;
+@property (nonatomic, retain) NSString* name;
+
+
+@end
